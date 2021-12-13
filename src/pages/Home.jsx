16f200c11cc122fab05cd47f48a81be16a9770fb
kamiaiqiaodaima <954108736@qq.com>
2021-12-13 17:32:07 +0800

    添加背景和鸟

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ class Home extends Component {
   render() {
     return (
       <div className="cardBox">
+        <a className="bird" />
          {
            routesConfig.gameMenus.map((menu) => (
            <Card
