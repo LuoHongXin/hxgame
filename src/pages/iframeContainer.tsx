a67334bbc853d9c6be0df1cb44748697e6e75b2d
kamiaiqiaodaima <954108736@qq.com>
2021-12-18 16:44:48 +0800

    iframe加载过程中添加loading效果

--- src/pages/iframeContainer.tsx
+++ src/pages/iframeContainer.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import routesConfig from '@/routes';
 import {
   withRouter,
 } from 'react-router-dom';
+import { Spin } from 'antd';
 
 type Props = {
   location:{pathname:string}
@@ -17,6 +18,24 @@ const iframeContainer :React.FC<Props> = (props:Props) => {
       }
     });
   });
-  return <iframe src={menu.meta.url} style={{ width: '100%', height: '100%' }} frameBorder="0" />;
+  const nameRef = useRef(null);
+  useEffect(() => {
+    console.log(nameRef, 'nameRef');
+    const ifra = nameRef.current;
+    ifra.onload = function () { // 非IE
+      setLoad(false);
+    };
+    ifra.onreadystatechange = function () { // IE下的节点都有onreadystatechange这个事件
+      if (ifra.readyState === 'complete') {
+        setLoad(false);
+      }
+    };
+  });
+  const [loading, setLoad] = useState(true);
+  return <><Spin tip="游戏加载中..." size="large" spinning={loading} style={{
+    width: '100%', height: '100%', paddingTop: '25%', color: 'red', fontSize: '5em',
+  }}
+           />
+    <iframe ref={nameRef} src={menu.meta.url} style={{ width: '100%', height: '100%' }} frameBorder="0" /></>;
 };
 export default withRouter(iframeContainer);
